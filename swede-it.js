8221edd042f4607505d9129a9aeb61c02840ef7c
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-02 18:15:40 -0400

    Take size and h:w ratio into account to choose flags

--- swede-it.js
+++ swede-it.js
@@ -1,13 +1,22 @@
 const BLUE_SELECTOR = "body, div, table, ul, form";
-const YELLOW_SELECTOR = "p, header, h1, h2, h3, h4";
-const FLAG_SELECTOR = "span, td, li, button, input";
-const SKEW_SELECTOR = FLAG_SELECTOR
+const YELLOW_SELECTOR = "p, span, a, td, li, blockquote, pre, button, input, header, h1, h2, h3, h4";
+const FLAG_SELECTOR = YELLOW_SELECTOR;
+
 
 $(BLUE_SELECTOR).css("background", "#006aa7");
 $(YELLOW_SELECTOR).css("background", "#fecc00");
-$(SKEW_SELECTOR).css("transform", "rotate(1deg)");
 
-flags = $(FLAG_SELECTOR);
+
+flags = $(FLAG_SELECTOR).filter(function() {
+  el = $(this);
+  h = el.height();
+  w = el.width();
+  return (w > 40 && h > 25 &&   // not too small
+          w < 400 && h < 250 && // not too big
+          (w * 2) > h &&              // not too tall
+          w < (3 * h))          // not too wide
+});
+
 flags.css("background-image", 'url("https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Flag_of_Sweden.svg/1600px-Flag_of_Sweden.svg.png")');
 flags.css("background-size", "100% 100%");
 flags.css("background-repeat", "no-repeat");
