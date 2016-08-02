26aec7c716eb540b8ca52f6696a72bb6e7039b61
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-02 17:48:03 -0400

    Nicer jquery selector params

--- swede-it.js
+++ swede-it.js
@@ -1,4 +1,14 @@
-$("body, div").css("background", "#006aa7");
+const BLUE_SELECTOR = "body, div, table, ul, form";
+const YELLOW_SELECTOR = "p, header, h1, h2, h3, h4";
+const FLAG_SELECTOR = "span, td, li, button, input";
+const SKEW_SELECTOR = FLAG_SELECTOR
 
-$("span, header, h1, h2, h3, h4").css("background", "#fecc00");
-$("span, header, h1, h2, h3, h4").css("transform", "rotate(1deg)");
+$(BLUE_SELECTOR).css("background", "#006aa7");
+$(YELLOW_SELECTOR).css("background", "#fecc00");
+$(SKEW_SELECTOR).css("transform", "rotate(1deg)");
+
+flags = $(FLAG_SELECTOR);
+flags.css("background-image", 'url("https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Flag_of_Sweden.svg/1600px-Flag_of_Sweden.svg.png")');
+flags.css("background-size", "100% 100%");
+flags.css("background-repeat", "no-repeat");
+flags.css("background-color", "rgba(0, 0, 0, 0.5)");
